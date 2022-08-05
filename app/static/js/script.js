72fefe71bafec57b0737a013d129fc37fe3b06b1
Ezequiel Parziale <eparziale@gmail.com>
2022-08-05 19:32:08 -0300

    :rocket: update: change language function

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,3 +3,25 @@ window.setTimeout(function() {
         $(this).remove(); 
     });
 }, 5000);
+
+
+function change_language(locale){
+    var url = '/account/change_language'
+    var data = {locale: locale}
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+    .catch(error => console.log('error:', error))
+    .then(response => {
+        if (response.status){
+            console.log("OK")
+            location.reload();
+        }else{
+            console.log("NOT")
+        }
+    })
+}
