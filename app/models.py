6c9de1f5b9b7db5cb234e369ed166a4e847fe1d5
Ezequiel Parziale <eparziale@gmail.com>
2023-03-30 21:24:22 -0300

    :card_file_box: update: upgrade model to sqlalchemy 2

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta, timezone
+from typing import List
 
 import jwt
 from flask import current_app, redirect, request, url_for
 from flask_babel import format_datetime
 from flask_login import UserMixin, current_user
 from sqlalchemy import BOOLEAN, Column, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -24,31 +26,34 @@ def unauthorized():
 
 class User(db.Model, UserMixin):  # type: ignore  # noqa
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    username = Column(String(40), unique=True, nullable=False)
-    email = Column(String(120), unique=True, nullable=False)
-    password = Column(String(60), nullable=False)
-    created_at = Column(
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(40), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(60), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
         server_default=text("CURRENT_TIMESTAMP"),
         nullable=False,
     )
-    updated_at = Column(
+    updated_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
         server_default=text("CURRENT_TIMESTAMP"),
         nullable=False,
     )
-    confirmed = Column(BOOLEAN, default=False)
-    locale = Column(String, nullable=False, default=settings.DEFAULT_LANGUAGE)
-    timezone = Column(String, nullable=True)
-    last_seen = Column(
+    confirmed: Mapped[bool] = mapped_column(BOOLEAN, default=False)
+    locale: Mapped[str] = mapped_column(
+        String, nullable=False, default=settings.DEFAULT_LANGUAGE
+    )
+    timezone: Mapped[str] = mapped_column(String, nullable=True)
+    last_seen: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
         server_default=text("CURRENT_TIMESTAMP"),
         nullable=False,
     )
 
     def __repr__(self) -> str:
-        return f"{self.username} : {self.email} : {self.created_at}"
+        return f"User(id={self.id}, username={self.username}, email={self.email}, created_at={self.created_at})"  # noqa: E501
 
     def ping(self):
         self.last_seen = datetime.utcnow()
@@ -86,7 +91,7 @@ class User(db.Model, UserMixin):  # type: ignore  # noqa
             self.confirmed = True
             self.update()
             return True
-        except:
+        except:  # noqa: E722
             return None
 
     def to_dict(self):
@@ -156,7 +161,7 @@ class User(db.Model, UserMixin):  # type: ignore  # noqa
                 token, current_app.config["SECRET_KEY"], algorithms=["HS256"]
             )
             user_id = decode.get("user_id")
-        except:
+        except:  # noqa: E722
             return None
         return User.query.get(user_id)
 
@@ -178,28 +183,31 @@ user_role = Table(
 
 class Role(db.Model):  # type: ignore  # noqa
     __tablename__ = "roles"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(40), unique=True, nullable=False)
-    description = Column(String(120), nullable=False)
-    created_at = Column(
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(40), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(120), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
         server_default=text("CURRENT_TIMESTAMP"),
         nullable=False,
     )
-    updated_at = Column(
+    updated_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
         server_default=text("CURRENT_TIMESTAMP"),
         nullable=False,
     )
-    created_user = Column(Integer, ForeignKey("users.id"))
-    updated_user = Column(Integer, ForeignKey("users.id"))
-    permissions = db.relationship(
+    created_user: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    updated_user: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    permissions: Mapped[List["Permission"]] = relationship(
         "Permission", secondary=role_permission, backref="roles"
     )
-    users = db.relationship("User", secondary=user_role, backref="roles")
+    users: Mapped[List["User"]] = relationship(
+        "User", secondary=user_role, backref="roles"
+    )
 
     def __repr__(self) -> str:
-        return f"{self.name} : {self.permissions} : {self.users}"
+        return f"Role(id={self.id}, name={self.name}, description={self.description}, created_at={self.created_at}, permissions={self.permissions}, users={self.users})"  # noqa: E501
 
     def save(self):
         self.created_user = current_user.id
@@ -237,25 +245,26 @@ class Role(db.Model):  # type: ignore  # noqa
 
 class Permission(db.Model):  # type: ignore  # noqa
     __tablename__ = "permissions"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(40), unique=True, nullable=False)
-    description = Column(String(120), nullable=False)
-    color = Column(String(40), nullable=False, default="ffffff")
-    created_at = Column(
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(40), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(120), nullable=False)
+    color: Mapped[str] = mapped_column(String(40), nullable=False, default="ffffff")
+    created_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
         server_default=text("CURRENT_TIMESTAMP"),
         nullable=False,
     )
-    updated_at = Column(
+    updated_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
         server_default=text("CURRENT_TIMESTAMP"),
         nullable=False,
     )
-    created_user = Column(Integer, ForeignKey("users.id"))
-    updated_user = Column(Integer, ForeignKey("users.id"))
+    created_user: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    updated_user: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
 
     def __repr__(self) -> str:
-        return f"{self.name}"
+        return f"Permission(id={self.id}, name={self.name}, description={self.description}, color={self.color}, created_at={self.created_at})"  # noqa: E501
 
     def save(self):
         self.created_user = current_user.id
