0e5578dac71e052819db7ec9a7e188073f521e7a
Ezequiel Parziale <eparziale@gmail.com>
2023-04-07 12:08:09 -0300

    :wrench: improve: structure config

--- app/config.py
+++ app/config.py
@@ -1,25 +1,57 @@
-from pydantic import BaseSettings
+from datetime import timedelta
+from typing import Any, Dict, Optional
+
+from pydantic import BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
+    # Flask
     SECRET_KEY: str
-    SQLALCHEMY_DATABASE_URI: str
-    SQLALCHEMY_TRACK_MODIFICATIONS: str
+
+    # Site
+    ADMIN_EMAIL: str
+    SITE_NAME: str
+    MAX_LOGIN_ATTEMPTS: int = 5
+    LOCK_TIME: timedelta = timedelta(minutes=5)
+    BLOCK_TIME: timedelta = timedelta(minutes=1)
+    DEFAULT_LANGUAGE: str = "en"
+    LANGUAGES: list = ["en", "es"]
+    ROWS_PER_PAGE: int = 10
+
+    # Email
     MAIL_SERVER: str
     MAIL_PORT: str
     MAIL_USE_TLS: str
     MAIL_USERNAME: str
     MAIL_PASSWORD: str
-    ADMIN_EMAIL: str
-    ROWS_PER_PAGE: int = 10
+
+    # Cache
     CACHE_TYPE: str = "SimpleCache"
     CACHE_DEFAULT_TIMEOUT: int = 300
-    # SQLALCHEMY_ECHO = True
 
-    SITE_NAME: str
+    # Database
+    POSTGRES_HOSTNAME: str
+    POSTGRES_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
-    DEFAULT_LANGUAGE: str = "en"
-    LANGUAGES: list = ["en", "es"]
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
+    SQLALCHEMY_ECHO: bool = False
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_HOSTNAME"),
+            port=values.get("POSTGRES_PORT"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
 
     class Config:
         env_file = ".env"
